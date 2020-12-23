0f428bc52a0892d0487a0e2194ca843004f8e3d1
zuosy <zuosy@wedoctor.com>
2020-12-23 13:55:48 +0800

    tshark过滤

--- capture_http_info.sh
+++ capture_http_info.sh
@@ -1,1 +1,3 @@
+###添加过滤,避免别人的请求影响自己过滤 
+###sudo tshark -i any -n -t a -R http.request -T fields -e "frame.time" -e "ip.src" -e "http.request.method" -e "http.host"  -e "http.request.uri" -R  'http.request.uri matches "follow"'
 tshark -i any -n -t a -R http.request -T fields -e "frame.time" -e "ip.src" -e "http.request.method" -e "http.host"  -e "http.request.uri"
