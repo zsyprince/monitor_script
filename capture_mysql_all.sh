7e9482512727bf0ebf7c054936b9c244a4c5633f
zuosongyan <952960946@qq.com>
2024-07-26 09:13:57 +0800

    Update capture_mysql_all.sh

--- capture_mysql_all.sh
+++ capture_mysql_all.sh
@@ -1,2 +1,3 @@
 #tshark -s 512 -i any -n -f 'tcp port 3319' -R'mysql.query' -T fields -e mysql.query
-sudo tshark -s 512 -i any -d tcp.port==3319,mysql -T fields -e mysql.query -R'mysql.query'
+#sudo tshark -s 512 -i any -d tcp.port==3319,mysql -T fields -e mysql.query -R'mysql.query'
+sudo tshark -s 2048 -i any -Y "tcp.port==3306 && mysql.query" -T fields -e mysql.query
